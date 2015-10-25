4622c70d841ab6a4ff427d950de9b13fa97982db
Bryan <johnson.4652@gmail.com>
2015-10-25 14:14:06 -0400

    Removed test files from directory

--- Algorithms/Huffman/main.cpp
+++ Algorithms/Huffman/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "huffman.h"
 
 int main(int argc, const char * argv[]) {
@@ -7,28 +8,45 @@ int main(int argc, const char * argv[]) {
     if(argc < 2) {
         std::cout << "Invalid arguments." << std::endl;
     } else {
-        std::fstream readFile;
-        readFile.open(argv[1]);
-        if(readFile.is_open()) {
-            std::string line;
-            char* ptr;
-            while ( getline (readFile,line) )
-            {
-                ptr = &line[0u];
-                while(*ptr != '\0') {
-                    ++frequencies[*ptr++];
+        std::string fileName = argv[1];
+        if(fileName.find(".hzip") != std::string::npos) {
+            // decode
+            std::cout << "Decode file!" << std::endl;
+        } else if(fileName.find(".txt") != std::string::npos) {
+            // encode
+            std::fstream readFile;
+            readFile.open(argv[1]);
+            if(readFile.is_open()) {
+                std::string line;
+                char* ptr;
+                while ( getline (readFile,line) )
+                {
+                    ptr = &line[0u];
+                    while(*ptr != '\0') {
+                        ++frequencies[*ptr++];
+                    }
                 }
+                readFile.close();
             }
-            readFile.close();
+
+            huffman::Node* root = huffman::Tree(frequencies);
+            std::map<char, std::vector<char> > codes;
+            huffman::Compress(root, std::vector<char>(), codes);
+
+            huffman::PrintFrequencyTable(root, codes);
+            huffman::TreeAsStringPost(root);
+            std::cout << std::endl;
+
+            // get name of new file
+            std::string newFile = fileName.substr(0, fileName.find(".txt")) + ".hzip";
+            std::cout << "Output File Name: " << newFile << std::endl;
+
+
+
+
+        } else {
+            std::cout << "Invalid file type." << std::endl;    
         }
-
-        huffman::Node* root = huffman::Tree(frequencies);
-        std::map<char, std::vector<char> > codes;
-        huffman::Compress(root, std::vector<char>(), codes);
-
-        huffman::PrintFrequencyTable(root, codes);
-        huffman::TreeAsStringPost(root);
-        std::cout << std::endl;
     }
     return 0;
 }
